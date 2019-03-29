b4192524cd962f16b2598bb7a6e2925b8320a0b9
Minh Hieu <hieutm.bk@gmail.com>
2019-03-29 17:32:13 +0700

    update

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ from feature_transformer import FeatureTransformer
 import json
 from underthesea import ner
 from sklearn.externals import joblib
-import base64
+from crawl_wiki_summary import MySpider
+from scrapy.crawler import CrawlerProcess
+import wikipedia
+
+wikipedia.set_lang("vi")
 PORT = 5000
 
 app = Flask(__name__)
@@ -35,19 +39,35 @@ def ner_tag(str) :
 
 @app.route('/predict', methods=['GET'])
 def extract():
-    """Return the movie review sentiment score.
-    
-    Returns a JSON object :
-    {
-         "sentiment": "positive"
-    }
-    """
+
     if request.method == 'GET':
         str = request.args['str']
+        ner_word = ner(str)
+        word_search = ""
+        for i in range(len(ner_word)):
+
+            if (ner_word[i][3] in ["B-PER","I-LOC", "I-PER","B-LOC"]):
+                word_search = word_search + ner_word[i][0] + " ";
+        word_search.strip()
+
+        wiki = wikipedia.page(word_search)
+        process = CrawlerProcess({
+            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        })
+        process.crawl(MySpider, myurls=[
+            wiki.url
+        ])
+        process.start()
+
+        with open('data.json', 'r') as f:
+            datastore = json.load(f)
+
         result = {
             'str' : str,
             'predict': predict(pipe_line, str),
-            'ner' : ner_tag(str.encode('utf-8'))
+            'ner' : ner_tag(str.encode('utf-8')),
+            'summary' : wiki.summary,
+            'infor' : datastore
         }
         return json.dumps(result) 
 
