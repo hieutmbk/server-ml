af428f875d0a3af9e9be1de557c73391098a03ea
Minh Hieu <hieutm.bk@gmail.com>
2019-04-02 01:08:23 +0700

    update

--- app.py
+++ app.py
@@ -51,9 +51,11 @@ def extract():
             if( ( (ner_word[i][1]=='N') & (ner_word[i][3]=='O') ) | (ner_word[i][1]=='V')):
                 first_word = ner_word[i][0][0]
                 if(first_word.isupper()):
-                    if((ner_word[i][0] == "Quê") | (ner_word[i][1] == "A")):
+                    if(ner_word[i][0] == "Quê" ):
                         qa_word = qa_word + ner_word[i][0] + " ";
                     search_word = search_word + ner_word[i][0] + " ";
+				if (ner_word[i][1] == "A"):
+					qa_word = qa_word + ner_word[i][0] + " ";
                 else:
                     qa_word = qa_word + ner_word[i][0] + " ";
 
