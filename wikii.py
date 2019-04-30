3219b52487772a28c756687bd7ca0eb3ecc8f417
Minh Hieu <hieutm.bk@gmail.com>
2019-04-30 13:52:02 +0700

    ss

--- wikii.py
+++ wikii.py
@@ -2,5 +2,5 @@ import wikipedia
 from underthesea import ner
 from pyvi import ViTokenizer
 wikipedia.set_lang("vi")
-sentences = "Tôn giáo tại Việt Nam khá đa dạng, gồm có các nhánh Phật giáo như Đại thừa, Tiểu thừa, Hòa Hảo, một số nhánh Kitô giáo như Công giáo Rôma"
+sentences = "câu lạc bộ Barcelona vô địch C1 năm nào"
 print(ner(sentences))
